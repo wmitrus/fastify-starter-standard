0a7be3d6fd07025d3f34e53e9aa7b1db0cc9d1e7
Wojciech Mitrus <wmitrus@gmail.com>
2022-04-21 10:31:09 +0200

    Add tests for examples

---
+++ test/examples/example2.test.js
@@ -0,0 +1,20 @@
+import tap from 'tap'
+import Bork from '../../examples/example2.js'
+
+const myBork = new Bork()
+
+tap.test('Bound functions are bound to the class instance.', t => {
+  t.equal(myBork.boundFunction.call(undefined), 'bork', 'Default value of instanceProperty')
+  myBork.instanceProperty = 'newBork'
+  t.equal(myBork.boundFunction.call(undefined), 'newBork', 'Value of instanceProperty was changed')
+
+  t.end()
+})
+
+tap.test('Static function exists on the class', t => {
+  t.equal(Bork.staticFunction(), 'babelIsCool')
+  Bork.staticProperty = 'Standard v17 Is Cool'
+  t.equal(Bork.staticFunction(), 'Standard v17 Is Cool')
+
+  t.end()
+})
