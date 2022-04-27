6632f19fdbc109557cf44ac56b4fd6e166d1919c
Wojciech Mitrus <wmitrus@gmail.com>
2022-04-27 22:04:35 +0200

    Add fastify-helmet dpendency and configuration
    Feature contains also content security policy directives for fastify-swagger.

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import Fastify from 'fastify'
 import { createRequire } from 'module'
+import helmet from '@fastify/helmet'
 
 const fastify = Fastify({
   logger: true
 })
 
 const require = createRequire(import.meta.url)
-
+fastify.register(helmet, {
+  global: true,
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ['\'self\''],
+      styleSrc: ['\'self\'', '\'unsafe-inline\''],
+      imgSrc: ['\'self\'', 'data:', 'validator.swagger.io'],
+      scriptSrc: ['\'self\'', 'https: \'unsafe-inline\'']
+    }
+  }
+})
 fastify.register(require('@fastify/swagger'), {
   exposeRoute: true,
   routePrefix: '/docs'
