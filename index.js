36cb30c0cdd9795cfadcad8cf2475c73fa5ed929
Wojciech Mitrus <wmitrus@gmail.com>
2022-04-27 15:07:51 +0200

    Fix bugs in index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-// const http = require('http')
-// const config = require('node-config-ajv')
 import { createServer } from 'http'
-import config from 'node-config-ajv'
 
 const hostname = '127.0.0.1'
 const port = 3000
@@ -9,8 +6,6 @@ const port = 3000
 const server = createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
-  const dbConfig = config.get('Customer.dbConfig.host')
-  res.end('' + dbConfig)
 })
 
 server.listen(port, hostname, () => {
