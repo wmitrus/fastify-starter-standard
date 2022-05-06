b868d3baeb4381d8c1b88d98f22dd41f91d57de6
Wojciech Mitrus <wmitrus@gmail.com>
2022-05-06 09:08:11 +0200

    Add cache for /swapi/films/:id endpoint

--- src/api/swapi.js
+++ src/api/swapi.js
@@ -181,10 +181,23 @@ const swapi = async (fastify, options) => {
 
   fastify.get('/swapi/films/:id', swapiFilmItem, async (request, reply) => {
     try {
+      const { redis } = fastify
+      const key = request.url
       const id = request.params.id
-      const response = await fastify.fetch('https://swapi.dev/api/films/' + id)
-      const json = await response.json()
-      reply.send(json)
+
+      const cache = await redis.get(key)
+      console.log(JSON.stringify(key))
+
+      if (!cache) {
+        fastify.log.info("Couldn't find a value in cache. Fetching data from api.")
+        const response = await fastify.fetch('https://swapi.dev/api/films/' + id)
+        const json = await response.json()
+        redis.set(request.query.key, JSON.stringify(json), 'EX', 60 * 5)
+        reply.send(json)
+      } else {
+        fastify.log.info('Got a value from cache key: ', key)
+        reply.send(JSON.parse(cache))
+      }
     } catch (err) {
       console.log(err)
     }
