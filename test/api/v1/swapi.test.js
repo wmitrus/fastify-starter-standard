921a2755a84d38c25c7e196ffe78b5942309448c
Wojciech Mitrus <wmitrus@gmail.com>
2022-05-13 10:39:03 +0200

    test: Add swapi test for /api/v1/swapi endpoint

--- test/api/v1/swapi.test.js
+++ test/api/v1/swapi.test.js
@@ -1,11 +1,18 @@
 import { test } from 'tap'
-import build from './../..//helper.js'
+import build from './../../helper.js'
 
 test('default root route', async (t) => {
   const app = await build(t)
 
   const res = await app.inject({
-    url: '/'
+    url: '/api/v1/swapi'
   })
-  t.same(JSON.parse(res.payload), { hello: 'world' })
+  t.same(JSON.parse(res.payload), {
+    films: 'https://swapi.dev/api/films/',
+    people: 'https://swapi.dev/api/people/',
+    planets: 'https://swapi.dev/api/planets/',
+    species: 'https://swapi.dev/api/species/',
+    starships: 'https://swapi.dev/api/starships/',
+    vehicles: 'https://swapi.dev/api/vehicles/'
+  })
 })
