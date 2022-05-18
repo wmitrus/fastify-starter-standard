7757439c6ffaf7f21b707c904d0c662f1fc98e91
Wojciech Mitrus <wmitrus@gmail.com>
2022-05-18 10:27:33 +0200

    test: Add test for all routes

--- test/api/v1/swapi.test.js
+++ test/api/v1/swapi.test.js
@@ -1,20 +1,61 @@
 import { test } from 'tap'
 import build from './../../helper.js'
 
-// test('Testing swapi', async () => {
-//   test('Default root route', async (t) => {
-//     const app = await build(t)
-//     console.log('APP AJV: ' + app.ajv)
-//     const res = await app.inject({
-//       url: '/api/v1/swapi'
-//     })
-//     t.same(JSON.parse(res.payload), {
-//       films: 'https://swapi.dev/api/films/',
-//       people: 'https://swapi.dev/api/people/',
-//       planets: 'https://swapi.dev/api/planets/',
-//       species: 'https://swapi.dev/api/species/',
-//       starships: 'https://swapi.dev/api/starships/',
-//       vehicles: 'https://swapi.dev/api/vehicles/'
-//     })
-//   })
-// })
+test('Testing swapi', async () => {
+  test('Default root route', async (t) => {
+    const app = await build(t)
+
+    const res = await app.inject({
+      url: '/api/v1/swapi'
+    })
+    t.same(res.statusCode, 200)
+    // t.same(JSON.parse(res.payload), {
+    //   films: 'https://swapi.dev/api/films/',
+    //   people: 'https://swapi.dev/api/people/',
+    //   planets: 'https://swapi.dev/api/planets/',
+    //   species: 'https://swapi.dev/api/species/',
+    //   starships: 'https://swapi.dev/api/starships/',
+    //   vehicles: 'https://swapi.dev/api/vehicles/'
+    // })
+  })
+
+  test('Films route', async (t) => {
+    const app = await build(t)
+
+    const res = await app.inject({
+      url: '/api/v1/swapi/films'
+    })
+
+    t.same(res.statusCode, 200)
+  })
+
+  test('Film route', async (t) => {
+    const app = await build(t)
+
+    const res = await app.inject({
+      url: '/api/v1/swapi/films/1'
+    })
+
+    t.same(res.statusCode, 200)
+  })
+
+  test('Planets route', async (t) => {
+    const app = await build(t)
+
+    const res = await app.inject({
+      url: '/api/v1/swapi/planets'
+    })
+
+    t.same(res.statusCode, 200)
+  })
+
+  test('Planet route', async (t) => {
+    const app = await build(t)
+
+    const res = await app.inject({
+      url: '/api/v1/swapi/planets/1'
+    })
+
+    t.same(res.statusCode, 200)
+  })
+})
